5f5fda5308662ac1c5cce507b2243dd284e153d3
Evan <evan@blockstudio.tw>
2021-02-24 19:10:42 +0800

    [feat] loading component

---
+++ components/Loading.vue
@@ -0,0 +1,22 @@
+<template>
+    <transition name="loading">
+        Loading
+    </transition>
+</template>
+
+<script>
+
+export default {
+    name: 'Loading',
+    methods: {
+        start () {
+        },
+        finish () {
+        }
+    }
+}
+</script>
+
+<style lang='scss'>
+
+</style>
